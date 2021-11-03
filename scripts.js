43d7c42ff7c86c0222e47e8426f8a1b3b71c729e
MHargrove96 <michaelh.96@yahoo.com>
2021-11-03 03:24:27 -0500

    finished

--- scripts.js
+++ scripts.js
@@ -1,15 +1,21 @@
+console.clear()
+
+
 let usersWord = ""
 let oddList = document.getElementById("odd")
 let evenList = document.getElementById("even")
+let newListItem = document.createElement(`li`)
 
 function updateWord(element) {
     usersWord = element.value
-    console.log("element is "+element)
-    console.log("userWord is "+usersWord)
+    console.log(element)
+    console.log(usersWord)
 }
+console.log(evenList)
+console.log(oddList)
 
-function handleSubmit(newListItem) {
-    let newListItem = document.createElement(`LI`)  
+function handleSubmit() {
+    console.log(newListItem)
     newListItem.innerText = usersWord
     if(usersWord.length % 2 === 0) {
         evenList.appendChild(newListItem)
@@ -19,17 +25,3 @@ function handleSubmit(newListItem) {
     usersWord = ""
     document.getElementById("even-odd-form").reset()
 }
-
-
-
-
-
-    // this line creates a new LI Element. We'll do this every 
-    // time a word is submitted so this should stay the same 
-    // this line sticks the user's word into the newly create
-    // LI Element so we'll keep this the same, as well.
-    // this line attaches/appends the element to the Odd List 
-    // !!!THIS IS WHERE THE PROGRAM SHOULD DECIDE IF IT'S EVEN OR ODD!!!
-    // puts word into even list 
-    // puts word into the odd list 
-    // these two lines will be the same
